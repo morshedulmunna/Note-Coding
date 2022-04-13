e3a1fa118d8ed0016720ed3a2e36bc2540c84964
Morshedul Munna <44342051+morshedulmunna@users.noreply.github.com>
2022-04-13 11:40:08 +0600

    Update and rename cp_c++ to cp.cpp

---
+++ cp.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+typedef long long int ll;
+const unsigned int MOD = 1000000007;
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif
+
+
+// code here______________
+
+#ifndef ONLINE_JUDGE
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+    return 0;
+}
